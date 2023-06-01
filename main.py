1438a084ea532a0b8549d576b50150459ab3ba38
andrik <mord.71gamer@gmail.com>
2023-06-01 13:07:57 -0600

    Entrega final Individual

--- main.py
+++ main.py
@@ -1,151 +1,231 @@
-from PyQt5.QtWidgets import QApplication, QMainWindow, QWidget, QVBoxLayout, QHBoxLayout, QPushButton, QFileDialog, QLabel, QFrame
 import sys
 import json
-import matplotlib.pyplot as plt
 import networkx as nx
-from PyQt5.QtWidgets import QWidget, QVBoxLayout
+import matplotlib.pyplot as plt
+from PyQt5.QtWidgets import QApplication, QMainWindow, QVBoxLayout, QWidget, QPushButton, QFileDialog, QMessageBox, QHBoxLayout
+from PyQt5.QtCore import QTimer, QEventLoop, Qt
 from matplotlib.backends.backend_qt5agg import FigureCanvasQTAgg as FigureCanvas
 
 
-class MinimumSpanningTreeTab(QWidget):
+class GraphWindow(QMainWindow):
     def __init__(self):
         super().__init__()
 
-        self.layout = QVBoxLayout()
-        self.setLayout(self.layout)
+        self.graph = None
+        self.tree_edges = None
+        self.current_edge_index = 0
+        self.node_positions = None
+        self.completed = False
 
-        # Crear una figura de Matplotlib y un lienzo de figura de Qt
+        self.AlgorithmPrim = AlgorithPrim()
+
+        self.setWindowTitle("Graph Viewer")
+        self.setFixedSize(1000, 700)  # tamaño fijo de la ventana
+
         self.figure = plt.figure()
         self.canvas = FigureCanvas(self.figure)
 
-        # Agregar el lienzo al diseño vertical
-        self.layout.addWidget(self.canvas)
-
-    def drawGraph(self, nodes, edges):
-        # Borrar el contenido de la figura antes de dibujar el grafo
-        self.figure.clear()
-
-        # Crear el grafo
-        graph = nx.Graph()
-
-        # Convertir los identificadores de nodos a cadenas
-        nodes = [str(node.get("id")) for node in nodes]
-
-        # Agregar los nodos al grafo
-        graph.add_nodes_from(nodes)
-
-        # Agregar las aristas al grafo y dibujar las conexiones con direcciones y pesos
-        for edge in edges:
-            source_node = str(edge.get("source"))
-            target_node = str(edge.get("target"))
-            weight = str(edge.get("weight", ""))
-
-            # Agregar la arista al grafo
-            graph.add_edge(source_node, target_node, weight=weight)
-
-        # Dibujar los nodos en la figura
-        pos = nx.spring_layout(graph)
-        nx.draw_networkx_nodes(graph, pos, node_color='lightblue')
-
-        # Dibujar las aristas (conexiones) en la figura con etiquetas de peso
-        nx.draw_networkx_edges(
-            graph, pos, edge_color='black', arrows=True, arrowstyle='->')
-        labels = nx.get_edge_attributes(graph, 'weight')
-        nx.draw_networkx_edge_labels(graph, pos, edge_labels=labels)
-
-        # Dibujar los identificadores de los nodos
-        node_labels = {node: node for node in graph.nodes}
-        nx.draw_networkx_labels(
-            graph, pos, labels=node_labels, font_color='black')
-
-        # Actualizar el lienzo de la figura
-        self.canvas.draw()
-
-
-class MainWindow(QMainWindow):
-    def __init__(self):
-        super().__init__()
-
-        self.setWindowTitle("Graph Viewer")
-        self.setFixedSize(1000, 700)  # Tamaño fijo de la ventana principal
-
-        # Crear el widget del gráfico
-        self.graph_widget = MinimumSpanningTreeTab()
-
-        # Crear los botones del menú lateral
-        self.btn_find = QPushButton("Find")
-        self.btn_reset = QPushButton("Reset")
-        self.btn_new_graph = QPushButton("New Graph")
-
-        # Conectar los botones a sus respectivas funciones
-        self.btn_find.clicked.connect(self.find_button_clicked)
-        self.btn_reset.clicked.connect(self.reset_button_clicked)
-        self.btn_new_graph.clicked.connect(self.new_graph_button_clicked)
-
-        # Crear el layout del menú lateral
-        menu_layout = QVBoxLayout()
-        # Márgenes del layout left, top, right, bottom
-        menu_layout.setContentsMargins(1, 1, 1, 600)
-        menu_layout.setSpacing(10)  # Espacio entre los botones
-        menu_layout.addWidget(self.btn_find)
-        menu_layout.addWidget(self.btn_reset)
-        menu_layout.addWidget(self.btn_new_graph)
-
-        # Crear el layout principal donde se mostrara el grafo
+        # layour principal para el grafo
         main_layout = QHBoxLayout()
-        # Uso de factor de estiramiento para auto ajustar el tamaño del widget del grafo
-        main_layout.addWidget(self.graph_widget, 1)
+
+        # creación del layout para los botones
+        menu_layout = QVBoxLayout()
+
+        # alineación de los botones
+        menu_layout.setAlignment(Qt.AlignTop | Qt.AlignRight)
+
+        # creación de los botones
+        import_button = QPushButton("New Graph")
+        self.find_button = QPushButton("Find MST Prim")
+        self.reset_button = QPushButton("Reiniciar")
+        self.clear_button = QPushButton("Limpiar")
+
+        # conectar los botones con las funciones
+        import_button.clicked.connect(self.import_json)
+        self.find_button.clicked.connect(self.find_mst)
+        self.reset_button.clicked.connect(self.reset_animation)
+        self.clear_button.clicked.connect(self.clear_graph)
+
+        menu_layout.addWidget(import_button)
+        menu_layout.addWidget(self.find_button)
+        menu_layout.addWidget(self.reset_button)
+        menu_layout.addWidget(self.clear_button)
+
+        main_layout.addWidget(self.canvas, 1)
+
+        # agregar el layout de los botones al layout principal
         main_layout.addLayout(menu_layout)
 
-        # Crear el widget central y establecer el layout principal
-        central_widget = QWidget()
-        central_widget.setLayout(main_layout)
+        self.central_widget = QWidget()
+        self.central_widget.setLayout(main_layout)
+        self.setCentralWidget(self.central_widget)
 
-        # Desactivar el botón "Reset" por defecto
-        self.btn_reset.setEnabled(False)
-        # Desactivar el botón "Find" por defecto
-        self.btn_find.setEnabled(False)
+        self.timer = QTimer()
+        self.timer.timeout.connect(self.next_step)
 
-        # Establecer el widget central en la ventana principal
-        self.setCentralWidget(central_widget)
-
-    def find_button_clicked(self):
-        pass
-
-    def reset_button_clicked(self):
-        if self.graph_widget.figure.axes:  # Verificar si hay algún dibujo presente
-            self.graph_widget.figure.clear()  # Borrar el contenido de la figura
-            self.graph_widget.canvas.draw()  # Actualizar el lienzo de la figura
-            self.btn_reset.setEnabled(False)  # Desactivar el botón "Reset"
-
-    def new_graph_button_clicked(self):
-        # Al momento de hacer clic se debe abrir la ventana de escritorio para que solo permita cargar archivos json
-
-        options = QFileDialog.Options()
-        # Utilizar el diálogo de PyQt en lugar del nativo del sistema
-        options |= QFileDialog.DontUseNativeDialog
-
-        file_filter = "JSON Files (*.json)"
-        file_path, _ = QFileDialog.getOpenFileName(
-            self, "Open JSON File", "", file_filter, options=options)
+    def import_json(self):
+        file_dialog = QFileDialog()
+        file_path, _ = file_dialog.getOpenFileName(
+            self, "Import JSON", "", "JSON Files (*.json)")
 
         if file_path:
-            # Cargar el archivo JSON
-            with open(file_path) as json_file:
-                data = json.load(json_file)
+            try:
+                with open(file_path, "r") as file:
+                    json_data = json.load(file)
+                    self.draw_graph(json_data)
+            except (FileNotFoundError, json.JSONDecodeError):
+                QMessageBox.warning(self, "Error", "Invalid JSON file.")
+        else:
+            QMessageBox.warning(self, "Error", "No file selected.")
 
-            # Obtener nodos y aristas del archivo JSON
-            nodes = data.get("nodes", [])
-            edges = data.get("edges", [])
+    def draw_graph(self, json_data):
+        self.graph = nx.Graph()
 
-            # Dibujar los nodos y conexiones en el GraphWidget
-            self.graph_widget.drawGraph(nodes, edges)
+        for node in json_data["nodes"]:
+            self.graph.add_node(node["id"])
 
-            self.btn_reset.setEnabled(True)
+        for edge in json_data["edges"]:
+            self.graph.add_edge(
+                edge["source"], edge["target"], weight=edge["weight"])
+
+        self.node_positions = nx.spring_layout(self.graph)
+
+        self.figure.clear()
+        nx.draw(self.graph, with_labels=True,
+                pos=self.node_positions, ax=self.figure.gca())
+        self.canvas.draw()
+
+    def find_mst(self):
+        if self.graph is None:
+            QMessageBox.warning(self, "Error", "Datos del grafo no validos")
+            return
+
+        if self.tree_edges is None:
+            self.tree_edges = AlgorithPrim.prim_mst(self.graph)
+            self.current_edge_index = 0
+            self.completed = False
+
+        if self.completed:
+            QMessageBox.information(
+                self, "Listo", "Minimum Spanning Tree encontrado.")
+            return
+
+        self.next_step()
+
+    def next_step(self):
+        if self.tree_edges is None:
+            return
+
+        if self.current_edge_index >= len(self.tree_edges):
+            self.current_edge_index = len(self.tree_edges) - 1
+            self.completed = True
+
+            QMessageBox.information(
+                self, "Listo", "Minimum Spanning Tree encontrado.")
+            return
+
+        self.highlight_edges()
+
+        self.current_edge_index += 1
+
+        # Pausa de 3 segundos antes de la siguiente animación
+        QTimer.singleShot(2000, self.next_step)
+
+    def highlight_edges(self):
+        if self.tree_edges is None:
+            return
+
+        self.figure.clear()
+
+        node_colors = ["gray" for _ in range(len(self.graph.nodes))]
+
+        # Colorea los nodos que están en el árbol de expansión mínima
+        for i, edge in enumerate(self.tree_edges):
+            if i <= self.current_edge_index:
+                source, target = edge
+                node_colors[list(self.graph.nodes).index(source)] = "blue"
+                node_colors[list(self.graph.nodes).index(target)] = "blue"
+
+        nx.draw_networkx_nodes(
+            self.graph, self.node_positions, node_color=node_colors, node_size=400)
+        nx.draw_networkx_labels(self.graph, self.node_positions)
+        nx.draw_networkx_edges(
+            self.graph, self.node_positions, edge_color="black", width=1, arrows=True, arrowstyle="->")
+
+        # Dibuja el peso de las aristas
+        edge_labels = {}
+        for source, target, data in self.graph.edges(data=True):
+            edge_labels[(source, target)] = data["weight"]
+
+        # Dibuja las etiquetas de las aristas
+        nx.draw_networkx_edge_labels(
+            self.graph, self.node_positions, edge_labels=edge_labels)
+
+        # Dibuja las aristas del árbol de expansión mínima
+        for i, edge in enumerate(self.tree_edges):
+            if i <= self.current_edge_index:
+                nx.draw_networkx_edges(self.graph, self.node_positions, edgelist=[
+                                       edge], edge_color="blue", width=1, arrows=True, arrowstyle="->")
+
+        # Dibuja los nodos del árbol de expansión mínima
+        self.canvas.draw()
+
+    # Reinicia la animación
+    def reset_animation(self):
+        if self.tree_edges is None:
+            return
+
+        self.current_edge_index = 0  # Reinicia el índice de la arista
+        self.completed = False  # Reinicia la flag de completado
+        self.highlight_edges()  # Dibuja el grafo original
+
+    # limpiar el grafo
+    def clear_graph(self):
+        self.graph = None
+        self.tree_edges = None
+        self.current_edge_index = 0
+        self.node_positions = None
+        self.completed = False
+        self.figure.clear()
+        self.canvas.draw()
+
+    # # Cierra la ventana
+    # def closeEvent(self, event):
+    #     super().closeEvent(event)
+
+
+class AlgorithPrim(QWidget):
+    def __init__(self):
+        pass
+
+    def prim_mst(graph):
+        tree_edges = []
+
+        # Elegir un nodo arbitrario como nodo inicial
+        start_node = list(graph.nodes)[0]
+
+        # Conjunto de nodos ya visitados
+        visited = set([start_node])
+
+        while len(visited) < len(graph.nodes):
+            min_edge = None
+            min_weight = float('inf')
+
+            # Buscar la arista de menor peso que conecta un nodo visitado con un nodo no visitado
+            for source, target, data in graph.edges(data=True):
+                if source in visited and target not in visited and data["weight"] < min_weight:
+                    min_edge = (source, target)
+                    min_weight = data["weight"]
+
+            if min_edge is not None:
+                tree_edges.append(min_edge)
+                visited.add(min_edge[1])
+
+        return tree_edges
 
 
 if __name__ == "__main__":
     app = QApplication(sys.argv)
-    window = MainWindow()
+    window = GraphWindow()
     window.show()
     sys.exit(app.exec_())
---
+++ main2.py
@@ -0,0 +1,152 @@
+from PyQt5.QtWidgets import QApplication, QMainWindow, QWidget, QVBoxLayout, QHBoxLayout, QPushButton, QFileDialog, QLabel, QFrame
+import sys
+import json
+import matplotlib.pyplot as plt
+import networkx as nx
+from PyQt5.QtWidgets import QWidget, QVBoxLayout
+from matplotlib.backends.backend_qt5agg import FigureCanvasQTAgg as FigureCanvas
+
+
+class MinimumSpanningTreeTab(QWidget):
+    def __init__(self):
+        super().__init__()
+
+        self.layout = QVBoxLayout()
+        self.setLayout(self.layout)
+
+        # Crear una figura de Matplotlib y un lienzo de figura de Qt
+        self.figure = plt.figure()
+        self.canvas = FigureCanvas(self.figure)
+
+        # Agregar el lienzo al diseño vertical
+        self.layout.addWidget(self.canvas)
+
+    def drawGraph(self, nodes, edges):
+        # Borrar el contenido de la figura antes de dibujar el grafo
+        self.figure.clear()
+
+        # Crear el grafo
+        graph = nx.Graph()
+
+        # Convertir los identificadores de nodos a cadenas
+        nodes = [str(node.get("id")) for node in nodes]
+
+        # Agregar los nodos al grafo
+        graph.add_nodes_from(nodes)
+
+        # Agregar las aristas al grafo y dibujar las conexiones con direcciones y pesos
+        for edge in edges:
+            source_node = str(edge.get("source"))
+            target_node = str(edge.get("target"))
+            weight = str(edge.get("weight", ""))
+
+            # Agregar la arista al grafo
+            graph.add_edge(source_node, target_node, weight=weight)
+
+        # Dibujar los nodos en la figura
+        pos = nx.spring_layout(graph)
+        nx.draw_networkx_nodes(graph, pos, node_color='lightblue')
+
+        # Dibujar las aristas (conexiones) en la figura con etiquetas de peso
+        nx.draw_networkx_edges(
+            graph, pos, edge_color='black', arrows=True, arrowstyle='->')
+        labels = nx.get_edge_attributes(graph, 'weight')
+        nx.draw_networkx_edge_labels(graph, pos, edge_labels=labels)
+
+        # Dibujar los identificadores de los nodos
+        node_labels = {node: node for node in graph.nodes}
+        nx.draw_networkx_labels(
+            graph, pos, labels=node_labels, font_color='black')
+
+        # Actualizar el lienzo de la figura
+        self.canvas.draw()
+
+
+class MainWindow(QMainWindow):
+    def __init__(self):
+        super().__init__()
+
+        self.setWindowTitle("Graph Viewer")
+        self.setFixedSize(1000, 700)  # Tamaño fijo de la ventana principal
+
+        # Crear el widget del gráfico
+        self.graph_widget = MinimumSpanningTreeTab()
+
+        # Crear los botones del menú lateral
+        self.btn_find = QPushButton("Find")
+        self.btn_reset = QPushButton("Reset")
+        self.btn_new_graph = QPushButton("New Graph")
+
+        # Conectar los botones a sus respectivas funciones
+        self.btn_find.clicked.connect(self.find_button_clicked)
+        self.btn_reset.clicked.connect(self.reset_button_clicked)
+        self.btn_new_graph.clicked.connect(self.new_graph_button_clicked)
+
+        # Crear el layout del menú lateral
+        menu_layout = QVBoxLayout()
+        # Márgenes del layout left, top, right, bottom
+        menu_layout.setContentsMargins(1, 1, 1, 600)
+        menu_layout.setSpacing(10)  # Espacio entre los botones
+        menu_layout.addWidget(self.btn_find)
+        menu_layout.addWidget(self.btn_reset)
+        menu_layout.addWidget(self.btn_new_graph)
+
+        # Crear el layout principal donde se mostrara el grafo
+        main_layout = QHBoxLayout()
+
+        # Uso de factor de estiramiento para auto ajustar el tamaño del widget del grafo
+        main_layout.addWidget(self.graph_widget, 1)
+        main_layout.addLayout(menu_layout)
+
+        # Crear el widget central y establecer el layout principal
+        central_widget = QWidget()
+        central_widget.setLayout(main_layout)
+
+        # Desactivar el botón "Reset" por defecto
+        self.btn_reset.setEnabled(False)
+        # Desactivar el botón "Find" por defecto
+        self.btn_find.setEnabled(False)
+
+        # Establecer el widget central en la ventana principal
+        self.setCentralWidget(central_widget)
+
+    def find_button_clicked(self):
+        pass
+
+    def reset_button_clicked(self):
+        if self.graph_widget.figure.axes:  # Verificar si hay algún dibujo presente
+            self.graph_widget.figure.clear()  # Borrar el contenido de la figura
+            self.graph_widget.canvas.draw()  # Actualizar el lienzo de la figura
+            self.btn_reset.setEnabled(False)  # Desactivar el botón "Reset"
+
+    def new_graph_button_clicked(self):
+        # Al momento de hacer clic se debe abrir la ventana de escritorio para que solo permita cargar archivos json
+
+        options = QFileDialog.Options()
+        # Utilizar el diálogo de PyQt en lugar del nativo del sistema
+        options |= QFileDialog.DontUseNativeDialog
+
+        file_filter = "JSON Files (*.json)"
+        file_path, _ = QFileDialog.getOpenFileName(
+            self, "Open JSON File", "", file_filter, options=options)
+
+        if file_path:
+            # Cargar el archivo JSON
+            with open(file_path) as json_file:
+                data = json.load(json_file)
+
+            # Obtener nodos y aristas del archivo JSON
+            nodes = data.get("nodes", [])
+            edges = data.get("edges", [])
+
+            # Dibujar los nodos y conexiones en el GraphWidget
+            self.graph_widget.drawGraph(nodes, edges)
+
+            self.btn_reset.setEnabled(True)
+
+
+if __name__ == "__main__":
+    app = QApplication(sys.argv)
+    window = MainWindow()
+    window.show()
+    sys.exit(app.exec_())
